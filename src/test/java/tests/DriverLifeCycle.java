0fadda598f72be54a2712706d8d7808e2c95baa8
mohammad hossein azizi <mhazizi7.mha@gmail.com>
2026-05-27 15:06:38 +0200

    initial commit

---
+++ src/test/java/tests/DriverLifeCycle.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverLifeCycle {
+    
+protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp(){
+        // declaration and instantiation of objects/variables
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+    }
+
+    @AfterEach
+    public void teardown(){
+        // close Chrome
+        driver.close();
+    }
+}
